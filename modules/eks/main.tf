7d12aab2cd8c0c25bd826cf86b31afecceb4ebe1
Ernesto Deluquez Heilbron <edeluquez@hotmail.com>
2025-02-24 01:13:29 +0000

    ✨feat(eks): add-ons, vpc tags and permissions

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -2,8 +2,9 @@
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
-  cluster_name    = "${terraform.workspace}-eks"
-  cluster_version = var.cluster_version
+  cluster_name           = "${terraform.workspace}-eks"
+  cluster_version        = var.cluster_version
+  cluster_upgrade_policy = "standard"
 
   vpc_id                   = var.vpc_id
   subnet_ids               = var.private_subnet_ids
@@ -12,6 +13,16 @@ module "eks" {
   enable_irsa                          = true
   cluster_endpoint_public_access       = true
   cluster_endpoint_public_access_cidrs = length(trimspace(var.allowed_ips)) > 0 ? split(",", trimspace(var.allowed_ips)) : []
+
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_addons = {
+    coredns                      = { addon_version = "v1.11.4-eksbuild.2" }
+    kube-proxy                   = { addon_version = "v1.32.0-eksbuild.2" }
+    aws-ebs-csi-driver           = { addon_version = "v1.39.0-eksbuild.1" }
+    aws-efs-csi-driver           = { addon_version = "v2.1.4-eksbuild.1" }
+    aws-mountpoint-s3-csi-driver = { addon_version = "v1.11.0-eksbuild.1" }
+  }
 
   eks_managed_node_groups = {
     default = {
