5fbd451752e8720711a1113ee8e3ed29776ec716
Ernesto Deluquez Heilbron <edeluquez@hotmail.com>
2025-02-28 14:15:02 +0000

    ✨Isolate repo infra

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,12 +1,29 @@
 
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-arm64-gp2"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+}
+
 module "ec2" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
-  create                      = var.power-on
+  create                      = var.wakeup
   name                        = "${terraform.workspace}-bastion"
-  instance_type               = "t2.nano"
+  ami                         = data.aws_ami.latest_amazon_linux.id
+  instance_type               = "t4g.nano"
   associate_public_ip_address = true
   subnet_id                   = var.public_subnet_ids[0]
+
   vpc_security_group_ids = [
     var.ssh_sg_id,
     var.postgresql_sg_id,
@@ -14,17 +31,15 @@ module "ec2" {
 
   user_data = <<EOF
 #!/bin/bash
-yum install -y nc tmux
-amazon-linux-extras install -y postgresql14 ansible2
-pip3 install --user psycopg2-binary
-ansible-galaxy collection install community.postgresql
-mkdir -p /home/ec2-user/.ssh
-cat <<EOK>> /home/ec2-user/.ssh/authorized_keys
-${join("\n", var.ssh_keys)}
-EOK
-chown -R ec2-user:ec2-user /home/ec2-user/.ssh
-chmod 500 /home/ec2-user/.ssh
-chmod 400 /home/ec2-user/.ssh/authorized_keys
+  yum install -y nc tmux
+  amazon-linux-extras install -y postgresql14 ansible2
+  mkdir -p /home/ec2-user/.ssh
+  cat <<EOK>> /home/ec2-user/.ssh/authorized_keys
+    ${join("\n", var.ssh_keys)}
+  EOK
+  chown -R ec2-user:ec2-user /home/ec2-user/.ssh
+  chmod 500 /home/ec2-user/.ssh
+  chmod 400 /home/ec2-user/.ssh/authorized_keys
   EOF
 
   tags = {
--- modules/ec2/variable.tf
+++ modules/ec2/variable.tf
@@ -1,6 +1,6 @@
 
+variable "wakeup" {}
 variable "ssh_keys" {}
-variable "power-on" {}
 variable "ssh_sg_id" {}
 variable "postgresql_sg_id" {}
 variable "public_subnet_ids" {}
